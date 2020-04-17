d9f4fff1f5aad57c09cba53ee113bf773a26b95c
Jason Righelato <jayontraps@gmail.com>
2020-04-17 09:36:38 +0100

    initial createRecord

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,78 @@
-const mutations = {};
+const { forwardTo } = require('prisma-binding');
+
+const mutations = {
+  async createRecord(parent, args, ctx, info) {
+    // temporarily asign a user to the observer field
+    const [me] = await ctx.db.query.users({
+      where: {
+        name: "Jason Righelato"
+      }
+    })
+
+    // temporarily aisign a location
+    const [loc] = await ctx.db.query.locations({
+      where: {
+        site: "Dinton marshes"
+      }
+    })
+
+    // temporarily aisign a species with bird class
+    const [sp] = await ctx.db.query.specieses({
+      where: {
+        class: {
+          name: "bird"
+        }
+      }
+    })
+
+    const item = await ctx.db.mutation.createRecord({
+      data: {
+        observer: {
+          connect: {
+            id: me.id
+          }
+        },
+        location: {
+          connect: {
+            id: loc.id
+          }
+        },
+        species: {
+          connect: {
+            id: sp.id
+          }
+        },
+        ...args
+      }
+    }, info)
+
+    return item
+  },
+  async addSpecies(parent, args, ctx, info) {       
+    // query for the types and set all new species to a bird for now
+    const [bird] = await ctx.db.query.classes({
+      where: {
+        name: "bird"
+      }
+    })
+
+    const item = await ctx.db.mutation.createSpecies({            
+      data: {      
+        class: {
+          connect: { 
+            id: bird.id 
+          },
+        },
+        ...args
+      }
+    }, info);
+
+    return item
+
+  },  
+  createLocation: forwardTo('db'),
+  createClass: forwardTo('db'),
+  createUser: forwardTo('db')
+}
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    species: forwardTo('db'),
+    classes: forwardTo('db'),
+    specieses: forwardTo('db'),
+    locations: forwardTo('db'),
+    async allSpecies(parent, args, ctx, info) {
+        const items = await ctx.db.query.specieses();
+        return items
+    },
+
+};
 
 module.exports = Query;
