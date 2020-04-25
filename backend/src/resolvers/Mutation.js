16e0f982142ce84647adb9b41db11923afb514e1
Jason Righelato <jayontraps@gmail.com>
2020-04-25 10:08:25 +0100

    update schema and add database prisma export zip

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,76 +1,76 @@
 const { forwardTo } = require('prisma-binding');
 
 const mutations = {
-  async createRecord(parent, args, ctx, info) {
-    console.log(args)
-    // temporarily asign a user to the observer field
-    // const [me] = await ctx.db.query.users({
-    //   where: {
-    //     name: "Jason Righelato"
-    //   }
-    // })
+  // async addRecord(parent, args, ctx, info) {
+  //   console.log(args)
+  //   // temporarily asign a user to the author field
+  //   // const [me] = await ctx.db.query.users({
+  //   //   where: {
+  //   //     name: "Jason Righelato"
+  //   //   }
+  //   // })
 
-    // if no location id provided, create a new one
-    let location
-    if (args.location.id) {
-      location = {
-        connect: {
-          id: args.location.id
-        }
-      }
-    } else {
-      location = {
-        create: {
-          site: args.location.site
-        }
-      }
-    }
+  //   // if no location id provided, create a new one
+  //   let location
+  //   if (args.location.id) {
+  //     location = {
+  //       connect: {
+  //         id: args.location.id
+  //       }
+  //     }
+  //   } else {
+  //     location = {
+  //       create: {
+  //         site: args.location.site
+  //       }
+  //     }
+  //   }
 
-    // if no observer id provided, create a new one
-    let observer
-    if (args.observer.id) {
-      observer = {
-        connect: {
-          id: args.observer.id
-        }
-      }
-    } else {
-      observer = {
-        create: {
-          name: args.observer.name
-        }
-      }
-    }
+  //   // if no author id provided, create a new one
+  //   let author
+  //   if (args.author.id) {
+  //     author = {
+  //       connect: {
+  //         id: args.author.id
+  //       }
+  //     }
+  //   } else {
+  //     author = {
+  //       create: {
+  //         name: args.author.name
+  //       }
+  //     }
+  //   }
 
-    // temporarily aisign a species with bird class
-    const [sp] = await ctx.db.query.specieses({
-      where: {
-        class: {
-          name: "bird"
-        }
-      }
-    })
+  //   // temporarily aisign a species with bird class
+  //   const [sp] = await ctx.db.query.specieses({
+  //     where: {
+  //       class: {
+  //         name: "bird"
+  //       }
+  //     }
+  //   })
 
-    // asign all new records to DRAFT status
-    const status = "DRAFT"
+  //   // asign all new records to DRAFT status
+  //   const status = "DRAFT"
     
-    const item = await ctx.db.mutation.createRecord({
-      data: {
-        species: {
-          connect: {
-            id: sp.id
-          }
-        },
-        ...args,
-        status,
-        location,
-        observer
-      }
-    }, info)
+  //   const item = await ctx.db.mutation.createRecord({
+  //     data: {
+  //       species: {
+  //         connect: {
+  //           id: sp.id
+  //         }
+  //       },
+  //       ...args,
+  //       status,
+  //       location,
+  //       author
+  //     }
+  //   }, info)
 
-    return item
+  //   return item
 
-  },
+  // },
   async addSpecies(parent, args, ctx, info) {       
     // query for the types and set all new species to a bird for now
     const [bird] = await ctx.db.query.classes({
@@ -93,6 +93,10 @@ const mutations = {
     return item
 
   },  
+  createSpeciesStatus: forwardTo('db'),
+  createBreedingCode: forwardTo('db'),
+  createRecord: forwardTo('db'),
+  createSpecies: forwardTo('db'),
   createLocation: forwardTo('db'),
   createClass: forwardTo('db'),
   createUser: forwardTo('db')
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,11 +1,13 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
+    users: forwardTo('db'),
     records: forwardTo('db'),
     species: forwardTo('db'),
     classes: forwardTo('db'),
     specieses: forwardTo('db'),
     locations: forwardTo('db'),
+    breedingCodes: forwardTo('db'),
     async allSpecies(parent, args, ctx, info) {
         const items = await ctx.db.query.specieses();
         return items
