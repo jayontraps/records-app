24b39d6b96d658ee2924a1d3dd1f9a99fc9d323b
Jason Righelato <jayontraps@gmail.com>
2020-06-08 12:26:09 +0100

    DOMAIN env var

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -50,7 +50,7 @@ const mutations = {
     ctx.response.cookie("token", token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365,
-      domain: process.env.FRONTEND_URL,
+      domain: process.env.DOMAIN,
     });
 
     return user;
